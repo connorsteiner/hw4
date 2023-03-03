9fe9397c7114ad61256d7f4e179402f2f8741c84
connorsteiner <connor.steiner@kellogg.northwestern.edu>
2023-03-03 17:31:51 +0000

    Updates to final assignment

--- app/controllers/places_controller.rb
+++ app/controllers/places_controller.rb
@@ -5,8 +5,13 @@ class PlacesController < ApplicationController
   end
 
   def show
-    @place = Place.find_by({ "id" => params["id"] })
-    @posts = Post.where({ "place_id" => @place["id"] })
+    if @current_user
+      @place = Place.find_by({ "id" => params["id"] })
+      @posts = Post.where({ "place_id" => @place["id"], "user_id" => @current_user["id"] })
+    else
+      flash["notice"] = "Login first."
+      redirect_to "/login"
+    end
   end
 
   def new
